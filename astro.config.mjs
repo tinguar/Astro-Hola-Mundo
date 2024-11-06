237284fb4f7f7df2d06968b6f2f6d5b4acabcbc3
Alberto Gabriel Guaman Garcia <80141815+GbrielGarcia@users.noreply.github.com>
2024-11-05 20:30:51 -0500

    REF: README

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Reemplaza con el nombre de tu repositorio
+    base: '/Astro-Hola-Mundo/',
+    output: 'static',
+});
